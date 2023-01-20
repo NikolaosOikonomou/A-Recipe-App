f4274d1549bce8d37c6a6034827c6a511e183c58
git_Nikos <nikosoikonomou@gmail.com>
2023-01-20 22:57:33 +0200

    Implement Log in methodology

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
 
-import { AuthService } from './auth.service';
+import { AuthResponseData, AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -10,6 +11,7 @@ import { AuthService } from './auth.service';
 export class AuthComponent {
   isLoginMode: boolean = true;
   isLoading: boolean = false;
+  isLoggedIn: boolean = false;
   error: string | null = null;
 
   constructor(private authService: AuthService) {}
@@ -23,24 +25,29 @@ export class AuthComponent {
     }
     const email = form.value.email;
     const password = form.value.password;
+    let authObs: Observable<AuthResponseData>;
     this.isLoading = true;
+
     if (this.isLoginMode) {
-      //
+      authObs = this.authService.logIn(email, password);
     } else {
-      this.authService.signUp(email, password).subscribe({
-        next: (response) => {
-          console.log(response);
-        },
-        error: errorMessage => {
-          console.log(errorMessage);
-          this.error = errorMessage;
-          this.isLoading = false;
-        },
-        complete: () => {
-          this.isLoading = false;
-        },
-      });
+      authObs = this.authService.signUp(email, password);
     }
+
+    authObs.subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (errorMessage) => {
+        console.log(errorMessage);
+        this.error = errorMessage;
+        this.isLoading = false;
+      },
+      complete: () => {
+        this.isLoading = false;
+        this.isLoggedIn = true;
+      },
+    });
     form.reset();
   }
 
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,13 +3,14 @@ import { Injectable } from '@angular/core';
 import { catchError, throwError } from 'rxjs';
 
 //Define how the response will look like. Documatation
-interface AuthResponseData {
-  kind: string,
+export interface AuthResponseData {
+  kind: string;
   idToken: string;
   email: string;
   refreshToken: string;
   expiresIn: string;
   localId: string;
+  registered?: boolean;
 }
 
 @Injectable({ providedIn: 'root' })
@@ -18,24 +19,40 @@ export class AuthService {
   constructor(private http: HttpClient) {}
 
   signUp(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' +
+          this.firebaseKey,
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(
+        catchError((errorRes) => {
+          let errorMessage = 'An unknown error occurred!';
+          if (!errorRes.error || !errorRes.error.error) {
+            return throwError(errorRes);
+          }
+          switch (errorRes.error.error.message) {
+            case 'EMAIL_EXISTS':
+              errorMessage = 'This email already exist.';
+          }
+          return throwError(errorMessage);
+        })
+      );
+  }
+
+  logIn(email: string, password: string) {
     return this.http.post<AuthResponseData>(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' +
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' +
         this.firebaseKey,
       {
         email: email,
         password: password,
         returnSecureToken: true,
       }
-    ).pipe(catchError(errorRes => {
-      let errorMessage = 'An unknown error occurred!';
-      if (!errorRes.error || !errorRes.error.error) {
-        return throwError(errorRes)
-      }
-      switch (errorRes.error.error.message) {
-        case 'EMAIL_EXISTS':
-          errorMessage = 'This email already exist.';
-      }
-      return throwError(errorMessage);
-    }))
+    );
   }
 }
